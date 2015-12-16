b342590fa8c17d21c9f48753d5a32a802fa4e3ec
jmechnich <joerg.mechnich@gmail.com>
2015-12-16 13:44:31 +0100

    get version from script in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup
 from codecs import open
 from os import path
+from luatool import version
 
 here = path.abspath(path.dirname(__file__))
 
+long_description = ''
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+
 setup(
     name='luatool',
-    version='0.1.0',
+    version=version,
     description='A tool for uploading lua scripts to the ESP8266 with nodemcu firmware.',
     long_description=long_description,
     url='https://github.com/4refr0nt/luatool',
